208da65072d37c425516cb6da716fdedb5c54aaa
yokikoshimizu <heppoko.yokiki@gmail.com>
2022-02-12 12:28:18 +0900

    .

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { useState } from 'react';
 
 function App() {
 
-  const todos =  []
+  //Reactで変数を使うときは、↓のようにuseStateを使って変数と関数(setter)を定義する
+  //値を更新するときはsetter関数を使って更新
+  // const todos =[] ←このようなjsの書き方だと、React上では再描画が行われない
+  const [todos, setTodos] = useState([])
+
   const addTodo = () => {
-    todos.push(1)
+    const newTodos = [...todos]
+    newTodos.push(1)
+    setTodos(newTodos)
     console.log(todos)
   }
 
